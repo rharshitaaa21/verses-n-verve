9a1de11c8a0f3272cb3bfc50cd89f2d5bfe8691a
rharshitaaa21 <raghav.harshita21@gmail.com>
2023-08-26 17:43:43 +0530

    Initial commit

---
+++ blog/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+# def index(request):
+#     pass
+def starting_page(request):
+    pass
+
+
+def posts(request):
+    pass
+
+
+def post_detail(request):
+    pass
