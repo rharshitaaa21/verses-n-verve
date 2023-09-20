db1f5ff868255c6fdd511614b442e48c44260f2e
rharshitaaa21 <raghav.harshita21@gmail.com>
2023-09-21 01:23:53 +0530

    added dynamic post content in front page

--- blog/models.py
+++ blog/models.py
@@ -4,11 +4,20 @@ from django.core.validators import MinLengthValidator
 # Create your models here
 class Tag(models.Model):
     caption = models.CharField(max_length = 20)
+    def __str__(self):
+        return f"{self.caption}" 
 
 class Author(models.Model): 
     first_name = models.CharField(max_length = 100)
     last_name = models.CharField(max_length=100)
     email_address = models.EmailField()
+
+    def fullname(self):
+        return f"{self.first_name}{self.last_name}"
+    
+    def __str__(self):
+        return self.fullname()
+    
 
 class Post(models.Model):
     title = models.CharField(max_length=150)
@@ -17,7 +26,7 @@ class Post(models.Model):
     date = models.DateField(auto_now = True)
     slug = models.SlugField(unique=True, db_index = True)
     content = models.TextField(validators=[MinLengthValidator(10)])
-    author = models.ForeignKey(Author, on_delete=models.SET_NULL, related_name="posts")
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL, related_name="posts", null = True)
     tags = models.ManyToManyField(Tag)
 
     
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from datetime import date
 
+from .models import Post
+
 
 
 all_posts = [
@@ -12,10 +14,6 @@ all_posts = [
         "title": "Mountain Hiking",
         "excerpt": "There's nothing like the views you get when hiking in the mountains! And I wasn't even prepared for what happened whilst I was enjoying the view!",
         "content": """
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
           aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
           velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
@@ -54,14 +52,7 @@ all_posts = [
         "title": "Nature At Its Best",
         "excerpt": "Nature is amazing! The amount of inspiration I get when walking in nature is incredible!",
         "content": """
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
+          
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
           aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
           velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
@@ -73,8 +64,9 @@ def get_date(post):
     return post['date']
 
 def starting_page(request):
-    sorted_posts = sorted(  all_posts,key = get_date)
-    latest_posts = sorted_posts[-3:]
+    latest_posts = Post.objects.all().order_by("-date")[:3]
+    # sorted_posts = sorted(  all_posts,key = get_date)
+    # latest_posts = sorted_posts[-3:]
     return render(request, "blog/index.html", {
         "posts": latest_posts
     })
@@ -91,3 +83,4 @@ def post_detail(request, slug):
     return render( request, "blog/post-detail.html", {
         "post" : identified_post
     })
+
